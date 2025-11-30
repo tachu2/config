9675a11db9a8afabf4b44414d35673f2ff57e77e
tachu <roatyungtatyu@gmail.com>
2025-11-30 14:42:05 +0900

    add: nvim config

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				-- sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+}
+
+-- return {
+--   -- add gruvbox
+--   { "ellisonleao/gruvbox.nvim" },
+--
+--   config = true,
+--   -- Configure LazyVim to load gruvbox
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent_mode = true,
+--       background = "dark",
+--       colorscheme = "gruvbox",
+--     },
+--   },
+-- }
